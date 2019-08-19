8be10c2ea68edce646848066c9db36483b66a591
splD <spldead@gmail.com>
2019-08-19 19:18:54 +0300

    добавил роут

--- src/server/index.js
+++ src/server/index.js
@@ -13,4 +13,8 @@ app.use(express.static('dist'));
 
 app.use('/api', apiRoutes);
 
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
